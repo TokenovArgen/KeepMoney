f6d9a6b6f9194fb90d0ec7cb7dec26faa94d046f
gxxxse <codingpong@gmail.com>
2021-06-22 10:50:37 +0600

    auth layout

---
+++ app/src/main/java/kg/token/keepmoney/AuthorizationActivity.kt
@@ -0,0 +1,16 @@
+package kg.token.keepmoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_authorization.*
+
+class AuthorizationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorization)
+        iv_google.setOnClickListener {
+
+        }
+    }
+}
